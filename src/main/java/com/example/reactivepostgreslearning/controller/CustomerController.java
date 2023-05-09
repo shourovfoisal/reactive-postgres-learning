48a2b05e97178f8d44294292366e9ca3235b1eed
Shourov Foisal <shourovfoisal@gmail.com>
2023-05-09 10:26:33 +0600

    Crud operation completed

--- src/main/java/com/example/reactivepostgreslearning/controller/CustomerController.java
+++ src/main/java/com/example/reactivepostgreslearning/controller/CustomerController.java
@@ -1,9 +1,11 @@
 package com.example.reactivepostgreslearning.controller;
 
+import com.example.reactivepostgreslearning.model.Customer;
 import com.example.reactivepostgreslearning.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/customer")
@@ -12,6 +14,35 @@ public class CustomerController {
 
     private final CustomerRepository repository;
 
-    
+    @GetMapping
+    public Flux<Customer> getCustomers() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Customer> getCustomer(@PathVariable Integer id) {
+        return repository.findById(id);
+    }
+
+    @PostMapping
+    public Mono<Customer> createCustomer(@RequestBody Customer customer) {
+        return repository.save(customer);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<Customer> updateCustomer(@RequestBody Customer customer,
+                               @PathVariable Integer id) {
+        return repository.findById(id)
+                .map(c -> {
+                    c.setName(customer.getName());
+                    return c;
+                })
+                .flatMap(repository::save);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deleteCustomer(@PathVariable Integer id) {
+        return repository.deleteById(id);
+    }
 
 }
